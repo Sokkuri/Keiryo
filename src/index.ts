e5668d65fc14c571c5e21d4cb16f73f78e13e635
TeraNovaLP <TeraNovaLP@protonmail.com>
2020-10-03 13:27:44 +0200

    Remove unused code

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-/*import InputComponent from "./components/InputComponent.vue";
-import SelectComponent from "./components/SelectComponent.vue";
-
-export {
-    InputComponent,
-    SelectComponent
-};*/
-
 export { default as InputComponent } from "./components/InputComponent.vue";
 export { default as SelectComponent } from "./components/SelectComponent.vue";
 export { default as SelectListItem } from "./models/SelectListItem";
+
