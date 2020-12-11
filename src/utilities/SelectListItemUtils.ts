dd01f03338091d16a16e5d4e3c9a5e025d25fc4d
TeraNovaLP <TeraNovaLP@protonmail.com>
2020-12-11 01:52:39 +0100

    Update SelectListItemUtils.ts

--- src/utilities/SelectListItemUtils.ts
+++ src/utilities/SelectListItemUtils.ts
@@ -6,15 +6,14 @@ import SelectListItem from "../models/SelectListItem";
 
 export default class SelectListItemUtils {
     public static getItems<T = string>(values: T[], labelSelector: (label: T) => string, valueSelector: (value: T) => string, selectedValues?: string[]) {
-        return values.map(x =>({
+        return values.map(x => ({
             label: labelSelector(x),
             value: valueSelector(x),
             selected: selectedValues?.some(y => this.equalsIgnoreCase(valueSelector(x), y))
         } as SelectListItem));
     }
 
-    private static equalsIgnoreCase(source: string, expected: string): boolean {
-        const pattern = new RegExp(expected, "i");
-        return pattern.test(source);
+    private static equalsIgnoreCase(source: string, expected: string) {
+        return source?.toLowerCase() == expected?.toLowerCase();
     }
 }
