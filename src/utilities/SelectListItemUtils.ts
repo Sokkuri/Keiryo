27e82e2fcb3e7bdd926acdea952775f99be0c170
TeraNovaLP <TeraNovaLP@protonmail.com>
2020-12-12 14:41:23 +0100

    Add update Selection to SelectListItemUtils

--- src/utilities/SelectListItemUtils.ts
+++ src/utilities/SelectListItemUtils.ts
@@ -2,6 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+import { Vue } from "vue-property-decorator";
 import SelectListItem from "../models/SelectListItem";
 
 export default class SelectListItemUtils {
@@ -12,6 +13,12 @@ export default class SelectListItemUtils {
                 value: valueSelector(x),
                 selected: selectedValues?.some(y => this.equalsIgnoreCase(valueSelector(x), y))
             } as SelectListItem));
+    }
+
+    public static updateSelection(items: SelectListItem[], selectedValues: string[]) {
+        items.forEach(x => {
+            Vue.set(items, items.indexOf(x), { ... x, selected: selectedValues.some(y => this.equalsIgnoreCase(x.value, y)) });
+        });
     }
 
     private static equalsIgnoreCase(source: string, expected: string) {
